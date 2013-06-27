7d098dcec8b597d8cf6bd03e1d306bef681c9160
stuehmer <roland.stuehmer@fzi.de>
2013-06-27 14:37:17 +0200

    Make exceptions less verbose

--- app/controllers/BackgroundTaskWebSocket.java
+++ app/controllers/BackgroundTaskWebSocket.java
@@ -14,12 +14,13 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 package controllers;
 
 import models.Message;
+import play.Logger;
 import play.libs.F.EventStream;
 import play.mvc.WebSocketController;
 
@@ -38,7 +39,12 @@ public class BackgroundTaskWebSocket extends WebSocketController {
 			Message message = await(liveStream.nextEvent());
 			if (message != null) {
 				String json = new Gson().toJson(message);
-				outbound.send(json);
+				try {
+					outbound.send(json);
+				}
+				catch (IllegalStateException e) {
+					Logger.debug(BackgroundTaskWebSocket.class.getSimpleName() + " Problem: " + e.getMessage());
+				}
 			}
 		}
 	}
