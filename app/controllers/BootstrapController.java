44a0dd6d3ea97aeca8e35e1c1ba361dbe9a1ccd6
stuehmer <roland.stuehmer@fzi.de>
2013-11-15 23:10:40 +0100

    Work around play!framework bug in Unbinder
    Avoid one call to subscriptionresult because controller calls cannot
    deal with lists in play! See
    http://play.lighthouseapp.com/projects/57987/tickets/1169-unbinder-doesnt-support-lists-of-objects
    for discussion
    
    Fixes play-project/play-governance-webapp#5

--- app/controllers/BootstrapController.java
+++ app/controllers/BootstrapController.java
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 package controllers;
@@ -40,7 +40,7 @@ public class BootstrapController extends PlayController {
 	/**
 	 * GET
 	 * 
-	 * Form display 
+	 * Form display
 	 * 
 	 * @param name
 	 * @param consumer
@@ -95,32 +95,22 @@ public class BootstrapController extends PlayController {
 			handleException("Locator error", e);
 		}
 
-		List<Subscription> subscriptions = null;
+		List<Subscription> bootSubscriptions = null;
 		try {
-			subscriptions = client.all();
+			bootSubscriptions = client.all();
 		} catch (GovernanceExeption e) {
 			e.printStackTrace();
 		}
 
-		List<Subscription> result = null;
-		if (subscriptions != null) {
+		List<Subscription> subscriptions = null;
+		if (bootSubscriptions != null) {
 			try {
-				result = subscriptionService.subscribe(subscriptions);
+				subscriptions = subscriptionService.subscribe(bootSubscriptions);
 			} catch (GovernanceExeption e) {
 				flash.error("Error while creating subscriptions '%s'",
 						e.getMessage());
 			}
 		}
-		subscriptionsResult(result);
-	}
-
-	/**
-	 * Display subscriptions
-	 * 
-	 * @param subscriptions
-	 */
-	public static final void subscriptionsResult(
-			List<Subscription> subscriptions) {
 		renderTemplate("BootstrapController/subscriptionsresult.html",
 				subscriptions);
 	}
